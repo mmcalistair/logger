3fc34a5934639bf681cd527beb5df813074c8088
Grischa Wende <admin@grischawen.de>
2016-05-05 18:47:40 +0200

    Adding dependencies

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 /**
  * Created by noname on 5/5/16.
  */
+
+var mongoClient = require('mongodb').MongoClient;
+var connectionUrl = 'mongodb://deb-dev:27017/test';
+mongoClient.connect(connectionUrl, function(err, db){
+    if(err){
+        console.error('Error while connecting');
+        console.log(err);
+    }else{
+        console.log('connected');
+        db.close();
+    }
+});
